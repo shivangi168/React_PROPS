0595cf4219d05637b60c2e8ea4774ff3af44aaea
Shivangi Shukla <64777788+shivangi168@users.noreply.github.com>
2023-02-04 18:36:06 +0530

    Create Sdata.jsx

---
+++ Sdata.jsx
@@ -0,0 +1,49 @@
+const Sdata=[
+    {
+    imgsrc:"https://wallpaperaccess.com/full/1605503.jpg",
+    title:" A Netflix Original  Series ",
+    sname:"Dark",
+    link:"https://www.netflix.com/in/title/80100172"
+
+    },
+    {
+  imgsrc:"https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg",
+  title:"A Netflix Original  Series",
+  sname:"Extra Curricular",
+  link:"https://www.netflix.com/in/title/80990668?source=35"
+  
+
+    },
+    {
+        imgsrc:"https://assets.gadgets360cdn.com/pricee/assets/product/202111/StrangerThings1-250_1636639727.jpg",
+        title:"A Netflix Original  Series",
+        sname:"Stranger Things",
+        link:"https://www.netflix.com/in/title/80057281"
+       
+
+    },
+    {
+        imgsrc:"https://i.pinimg.com/736x/58/71/52/587152adffc476276916f8e5ff0817bc.jpg",
+        title:"A Netflix Original  Series",
+        sname:"Lost in space",
+        link:"https://www.netflix.com/in/title/80104198"
+       
+
+    },
+    {
+        imgsrc:"https://ntvb.tmsimg.com/assets/p10700229_b_h10_aa.jpg?w=1280&h=720",
+        title:"A Netflix Original  Series",
+        sname:"Vampire Diaries",
+        link:"https://www.netflix.com/in/title/70143860"
+       
+    },
+    {
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BOTUzMTMzNTgtNmI5NS00ZTI1LTgzMjYtZTFkZWZkYTE0MWMyXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg",
+        title:"A Netflix Original  Series",
+        sname:"Mismatched",
+        link:"https://www.netflix.com/in/title/80220079"
+       
+
+    },
+];
+export default Sdata;
